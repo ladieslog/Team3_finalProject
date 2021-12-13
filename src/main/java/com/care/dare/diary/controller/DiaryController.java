2623a57c77e9ba96ccc3fdb0a5cd81a6f2205ed9
jinsung <tjd610@naver.com>
2021-12-13 15:58:42 +0900

    diaryWrite 화면, db 완료

--- src/main/java/com/care/dare/diary/controller/DiaryController.java
+++ src/main/java/com/care/dare/diary/controller/DiaryController.java
@@ -1,10 +1,17 @@
 package com.care.dare.diary.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import com.care.dare.diary.diaryService.DiaryService;
 
 @Controller
 public class DiaryController {
+	@Autowired DiaryService ds;
+	
 	@RequestMapping("diaryBoard")
 	public String diaryBoard() {
 		return "diary/diaryBoard";
@@ -19,4 +26,12 @@ public class DiaryController {
 	public String diaryView() {
 		return "diary/diaryView";
 	}
+	
+	@PostMapping("writeSave")
+	public String writeSave(MultipartHttpServletRequest mul) throws Exception {
+		ds.writeSave(mul);
+		return "home";
+	}
+	
+	
 }
