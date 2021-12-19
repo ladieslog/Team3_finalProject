3c54a8c4a9d3f0db46f17be482fb1edae5ca777c
KJH <wnsgh9978@naver.com>
2021-12-19 21:24:37 +0900

    notice 작성, 페이지, 뷰, 삭제 기능 완료

---
+++ src/main/java/com/care/dare/mybatis/CSMapper.java
@@ -0,0 +1,15 @@
+package com.care.dare.mybatis;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.care.dare.CS.DTO.NoticeDTO;
+
+public interface CSMapper {
+	public int noticeWrite(NoticeDTO dto);
+	public ArrayList<NoticeDTO> noticeBoard(@Param("s") int startRow, @Param("e") int endRow);
+	public int noticeCount();
+	public NoticeDTO noticeInfo(int num);
+	public int noticeDelete(int num);
+}
