911637d1d7586b57634afa68fe604d562b0a0cbd
ysshin <email@email.com>
2021-12-30 18:00:50 +0900

    21.12.30 18:00 커밋

--- src/main/webapp/resources/map/map.js
+++ src/main/webapp/resources/map/map.js
@@ -6,7 +6,6 @@ let mapOptions,
 	polyLineArray = [],
 	markerInfoArray = [],
 	marker,
-	markers = [],
 	infoWindows = [];
 
 
@@ -51,7 +50,7 @@ function makeArrayLocation() {	// ajax로 받은 데이터 중 location1, locati
 	}
 }
 
-function geocoding() {	// 주소를 좌표로 변환 (마커를 찍기 전 좌표 구하기)
+function geocoding(mapType) {	// 주소를 좌표로 변환 (마커를 찍기 전 좌표 구하기)
 
 	for (let i = 0; i < loactionArray.length; i++) {
 		if (loactionArray[i] != 'no Location!!') {
@@ -71,8 +70,7 @@ function geocoding() {	// 주소를 좌표로 변환 (마커를 찍기 전 좌�
 						geocodeArray[i] = items[0].point;
 
 						if (i == loactionArray.length - 1) {
-							markers.push(geocodeArray);
-							setTimeout("makeMarkers()", 500);
+							setTimeout(makeMarkers(mapType), 500)
 						}
 					}
 				});
@@ -80,7 +78,7 @@ function geocoding() {	// 주소를 좌표로 변환 (마커를 찍기 전 좌�
 	}
 }
 
-function makeMarkers() {	// 마커 찍기
+function makeMarkers(mapType) {	// 마커 찍기
 
 	let markerURL = "/root/resources/map/img/marker",
 		markerTail = ".png",
@@ -120,15 +118,35 @@ function makeMarkers() {	// 마커 찍기
 					url: markerURL + markerColor + markerTail,
 					anchor: new naver.maps.Point(15, 32)
 				},
-				zIndex: 800
 
+				zIndex: 800
+			});
+
+			var infoWindow = new naver.maps.InfoWindow({
+				content: '<div style="width:150px;text-align:center;padding:10px;">"' + marker.title + '"</b>.</div>'
 			});
 
 			markerInfoArray.push(marker);
+			infoWindows.push(infoWindow);
+
 		}
 	}
 
-	drawPolyline();
+	if (mapType == 2) {
+		for (let i = 0; i < markerInfoArray.length; i++) {
+			let markers = markerInfoArray[i];
+			markers.setMap(null);
+		}
+	}
+
+	else {
+		for (let i = 0; i < markerInfoArray.length; i++) {
+			naver.maps.Event.addListener(markerInfoArray[i], 'click', getClickHandler(i));
+
+		}
+
+		drawPolyline();
+	}
 }
 
 function drawPolyline() {	// 폴리라인 그리기
@@ -172,6 +190,7 @@ function drawPolyline() {	// 폴리라인 그리기
 					strokeColor: polyLineColor,
 					strokeOpacity: 1,
 					strokeWeight: 3,
+					zIndex: 800
 				})
 			}
 
@@ -190,14 +209,13 @@ function drawPolyline() {	// 폴리라인 그리기
 					strokeColor: polyLineColor,
 					strokeOpacity: 1,
 					strokeWeight: 3,
+					zIndex: 800
 				})
 			}
 		}
 
 		cnt++;
 	}
-
-	console.log(markerInfoArray);
 }
 
 function initMap() {
@@ -275,98 +293,98 @@ naver.maps.Event.once(map, 'init_stylemap', function() {
 	});
 });
 
-function viewSelected(naverCode) {
+function viewSelected(naverCode) {	// 선택한 도시로 이동
 
 	let coordinate,
 		zoomLevel;
 
 	if (naverCode == 0) {
-		coordinate = [35.797336021559566, 127.61588108068118];
+		coordinate = [35.79733, 127.61588];
 		zoomLevel = 7;
 	}
 
 	else if (naverCode == 1) {
-		coordinate = [37.77875952149849, 128.45614856831327];
+		coordinate = [37.77875, 128.45614];
 		zoomLevel = 9;
 	}
 
 	else if (naverCode == 2) {
-		coordinate = [37.58828996787081, 127.13976342063229];
+		coordinate = [37.58828, 127.13976];
 		zoomLevel = 9;
 	}
 
 	else if (naverCode == 3) {
-		coordinate = [35.312485951670965, 128.35904991977813];
+		coordinate = [35.31248, 128.35904];
 		zoomLevel = 9;
 	}
 
 	else if (naverCode == 4) {
-		coordinate = [36.43746228571184, 128.8171692594408];
+		coordinate = [36.43746, 128.81716];
 		zoomLevel = 9;
 	}
 
 	else if (naverCode == 5) {
-		coordinate = [35.161845215083254, 126.85388112266982];
+		coordinate = [35.16184, 126.85388];
 		zoomLevel = 11;
 	}
 
 	else if (naverCode == 6) {
-		coordinate = [35.8383974839094, 128.5668823913586];
+		coordinate = [35.83839, 128.56688];
 		zoomLevel = 11;
 	}
 
 	else if (naverCode == 7) {
-		coordinate = [36.34661102368089, 127.40585548936683];
+		coordinate = [36.34661, 127.40585];
 		zoomLevel = 11;
 	}
 
 	else if (naverCode == 8) {
-		coordinate = [35.173219844383105, 129.04001281778324];
+		coordinate = [35.17321, 129.04001];
 		zoomLevel = 11;
 	}
 
 	else if (naverCode == 9) {
-		coordinate = [37.558033800475805, 127.00829015424965];
+		coordinate = [37.55803, 127.00829];
 		zoomLevel = 11;
 	}
 
 	else if (naverCode == 10) {
-		coordinate = [35.55768272122909, 129.25686823889524];
+		coordinate = [35.55768, 129.25686];
 		zoomLevel = 11;
 	}
 
 	else if (naverCode == 11) {
-		coordinate = [37.59638345853803, 126.57388688320925];
+		coordinate = [37.59638, 126.57388];
 		zoomLevel = 9;
 	}
 
 	else if (naverCode == 12) {
-		coordinate = [34.85810739384981, 126.80346385232812];
+		coordinate = [34.85810, 126.80346];
 		zoomLevel = 9;
 	}
 
 	else if (naverCode == 13) {
-		coordinate = [35.6885872249758, 127.15127296373089];
+		coordinate = [35.68858, 127.15127];
 		zoomLevel = 9;
 	}
 
 	else if (naverCode == 14) {
-		coordinate = [33.376879301851545, 126.57380581097621];
+		coordinate = [33.37687, 126.57380];
 		zoomLevel = 11;
 	}
 
 	else if (naverCode == 15) {
-		coordinate = [36.50970606936858, 126.91170070309774];
+		coordinate = [36.50970, 126.91170];
 		zoomLevel = 9;
 	}
 
 	else if (naverCode == 16) {
-		coordinate = [36.640848853510505, 127.99312538689517];
+		coordinate = [36.64084, 127.99312];
 		zoomLevel = 9;
 	}
 
 	else if (naverCode == 17) {
-		coordinate = [36.569344588666596, 127.24219219853576];
+		coordinate = [36.56934, 127.24219];
 		zoomLevel = 11;
 	}
 
@@ -438,6 +456,10 @@ function regionJsonLoop(mapType) {
 						loadCount++;
 
 						if (loadCount == 267) {
+							if (mapType == 2 || mapType == 3 || mapType == 4) {
+								geocoding(mapType);
+							}
+
 							startDataLayer(mapType);
 						}
 					}
@@ -468,7 +490,7 @@ function startDataLayer(mapType) {
 					strokeColor: '#FFFFFF',
 					strokeWeight: 2,
 					strokeOpacity: 1,
-					zIndex : 500
+					zIndex: 500
 				};
 			}
 
@@ -479,7 +501,7 @@ function startDataLayer(mapType) {
 					strokeColor: '#FFFFFF',
 					strokeWeight: 1,
 					strokeOpacity: 0.5,
-					zIndex : 500
+					zIndex: 500
 				};
 			}
 
@@ -495,7 +517,7 @@ function startDataLayer(mapType) {
 							strokeColor: '#FFFFFF',
 							strokeWeight: 1,
 							strokeOpacity: 0.5,
-							zIndex : 500
+							zIndex: 500
 						};
 					}
 				}
@@ -510,7 +532,7 @@ function startDataLayer(mapType) {
 							strokeColor: '#FFFFFF',
 							strokeWeight: 1,
 							strokeOpacity: 0.5,
-							zIndex : 500
+							zIndex: 500
 						};
 					}
 				}
@@ -525,7 +547,7 @@ function startDataLayer(mapType) {
 					strokeColor: '#FFFFFF',
 					strokeWeight: 2,
 					strokeOpacity: 1,
-					zIndex : 500
+					zIndex: 500
 				};
 			}
 			else {
@@ -535,7 +557,7 @@ function startDataLayer(mapType) {
 					strokeColor: '#FFFFFF',
 					strokeWeight: 1,
 					strokeOpacity: 0.5,
-					zIndex : 500
+					zIndex: 500
 				};
 			}
 		}
@@ -547,9 +569,8 @@ function startDataLayer(mapType) {
 		map.data.addGeoJson(geojson);
 	});
 
-	if (mapType == 3 || mapType == 4) {
-		geocoding();
-	}
+
+
 
 	if (mapType != 4) {
 
@@ -557,6 +578,7 @@ function startDataLayer(mapType) {
 			let feature = e.feature,
 				naverCodeInt = parseInt(feature.getProperty('navercode'));
 
+			console.log("feature 11111111");
 			console.log(feature);
 
 			viewSelected(naverCodeInt);
@@ -579,12 +601,6 @@ function startDataLayer(mapType) {
 		});
 	}
 
-	else {
-		// 해당 마커의 인덱스를 seq라는 클로저 변수로 저장하는 이벤트 핸들러를 반환합니다.
-		for (let i = 0; i < markerInfoArray.length; i++) {
-			naver.maps.Event.addListener(markerInfoArray[i], 'click', getClickHandler(i));
-		}
-	}
 
 	map.data.addListener('mouseover', function(e) { // 마우스 올린 상태
 		let feature = e.feature,
@@ -660,17 +676,15 @@ function hideMarker(map, marker) {
 
 
 function getClickHandler(seq) {
-	console.log("clickclickclickclickclick");
-	console.log(seq);
-
+	console.log("getClickHandler 실행됨");
 	return function(e) {
 		let testmarker = markerInfoArray[seq],
 			infoWindow = infoWindows[seq];
-		console.log("markerInfoArray[i]markerInfoArray[i]markerInfoArray[i]");
-		/*if (infoWindow.getMap()) {
+
+		if (infoWindow.getMap()) {
 			infoWindow.close();
 		} else {
 			infoWindow.open(map, testmarker);
-		}*/
+		}
 	}
 }
