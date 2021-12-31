b3d8d0293aeb398154ac68a777795e2b2402cb55
KJH <wnsgh9978@naver.com>
2021-12-31 14:42:00 +0900

    31일 중간 커밋

--- src/main/java/com/care/dare/diary/controller/DiaryController.java
+++ src/main/java/com/care/dare/diary/controller/DiaryController.java
@@ -43,7 +43,6 @@ public class DiaryController {
 		resp.setContentType("text/html; charset=utf-8"); // 응답 설정 변경
         PrintWriter out = resp.getWriter(); // 화면 출력용 객체
 		HttpSession session = req.getSession();
-		System.out.println(session.getAttribute("loginUser"));
 		String search = req.getParameter("search");
 		if(search == null) {
 			search = "";
@@ -51,7 +50,7 @@ public class DiaryController {
 		if(session.getAttribute("loginUser")==null) {
 			return "redirect:error";
 		}else {
-			MemberDTO dto1 = (MemberDTO)session.getAttribute("loginUser");
+		MemberDTO dto1 = (MemberDTO)session.getAttribute("loginUser");
 		int pageSize = 0;
 		int diaryCount = ds.diaryCount(dto1.getId(),search);
 		model.addAttribute("diaryCount", diaryCount);
@@ -75,7 +74,6 @@ public class DiaryController {
 			endRow = pageNum * pageSize - 1;
 		}
 		
-		System.out.println(search);;
 		ArrayList<DiaryDTO> arr = ds.diaryBoard(startRow, endRow, dto1.getId(),search);
 		SimpleDateFormat sim = new SimpleDateFormat("yyyy-MM-dd");
 		int count = 0;
